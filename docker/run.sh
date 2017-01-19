2b5e81a17d6392c425d0d6ee92cf9a87714d24a2
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2017-01-19 09:56:40 +0000

    Docker run.sh variables must match the ones in doc and envitonment.in

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,7 @@ service_bus.uri=${SERVICE_BUS_URI:?}
 # UG propagator input queue - reader
 service_bus.user=${SERVICE_BUS_USER:?}
 service_bus.password=${SERVICE_BUS_PASSWORD:?}
-service_bus.ug.queue=${SERVICE_BUS_UG_QUEUE:?}
+service_bus.ug.queue=${SERVICE_BUS_QUEUE:?}
 eof
 
 if [ "$*" = "start" ]; then
